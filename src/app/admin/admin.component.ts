fa728b7efdeb072909b3112e43fe7e456ade5404
alejandro <alejocarreteroballesteros@gmail.com>
2023-10-14 14:47:57 -0500

    no quiero subir este commit

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent {
+  constructor(private router: Router) { }
+
+  OutSesion(): void {
+    this.router.navigate(['/']);
+    }
+}
