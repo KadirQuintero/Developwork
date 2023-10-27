0fbf931fec2e33416e4cc633d9b2b3f3bbebb73a
alejandro <alejocarreteroballesteros@gmail.com>
2023-10-27 08:02:02 -0500

    CRUD personas

---
+++ src/app/personas/m-personas/m-personas.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { persona } from '../../models/persona';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-m-personas',
+  templateUrl: './m-personas.component.html',
+  styleUrls: ['./m-personas.component.css'],
+})
+export class MPersonasComponent implements OnInit {
+  lstpersonas: persona[] = [];
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    fetch('../../assets/personas.json')
+      .then((Response) => Response.json())
+      .then((data) => {
+        data.forEach((Element: persona) => {
+          this.lstpersonas.push(Element);
+        });
+        this.lstpersonas.forEach((Element: persona) => {
+          console.log(Element);
+        });
+      });
+  }
+  register(): void {
+    this.router.navigate(['user/register']);
+  }
+}
