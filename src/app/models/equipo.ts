f81a4e18ba42fe6604566e12835fe4a8d1a2b6c7
KadirQuintero <keduardoquintero@unicesar.edu.co>
2023-11-09 09:01:32 -0500

    se agregaron los campos de Jornada, Estado, Rol en la tabla de Equipos

--- src/app/models/equipo.ts
+++ src/app/models/equipo.ts
@@ -1,11 +1,11 @@
-import { estado } from "./estado";
-import { jornada } from "./jordanas";
-import { rol } from "./rol";
-export class equipo{
-    id_equipo:string='';
-    nombre_equipo:string='';
-    descripcion?:string;
-    rol :rol =new rol();
-    jornada: jornada= new jornada();
-    estado:estado= new estado();
+import { estado } from './estado';
+import { jornada } from './jordanas';
+import { rol } from './rol';
+export class equipo {
+  id_equipo: string = '';
+  nombre_equipo: string = '';
+  descripcion?: string;
+  rol: rol = new rol();
+  jornada: jornada = new jornada();
+  estado: estado = new estado();
 }
